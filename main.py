885609f6f42092367a7700ea343950e90a1f2184
Marvin Duran <13426120+mduran17@users.noreply.github.com>
2023-12-19 12:55:48 -0500

    Create main.py
    fun with turtle party functions!

---
+++ main.py
@@ -0,0 +1,25 @@
+# Turtle Party Project
+# by Marvin Duran
+# 12/19/2023
+
+
+
+import turtle
+
+turtle.shape("turtle")
+turtle.speed(3)
+
+turtle.color("blue") #added
+turtle.width(5)
+
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(90)
+
+
+
+turtle.Screen().exitonclick()
